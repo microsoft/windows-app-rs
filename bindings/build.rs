ad2de906b49ca48b6a5b7d99b81826e6122303e0
Rafael Rivera <rafael@withinwindows.com>
2021-06-24 14:54:29 -0700

    Pare down build.rs

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,11 +1,5 @@
 fn main() {
     windows::build! {
-        Microsoft::UI::Xaml::Window,
-        Windows::Win32::Foundation::{BOOL, HWND, RECT},
-        Windows::Win32::UI::HiDpi::GetDpiForWindow,
-        Windows::Win32::UI::WindowsAndMessaging::{
-            GetSystemMetrics, GetWindowRect, MessageBoxW, SetWindowPos, MESSAGEBOX_STYLE,
-            SYSTEM_METRICS_INDEX,
-        },
+        Windows::Win32::UI::WindowsAndMessaging::MessageBoxW
     };
 }
