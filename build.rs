8d937510a161d3e4ccb2b00f8ede1f10613d7df6
Kenny Kerr <kenny@kennykerr.ca>
2021-07-01 11:11:55 -0700

    build

--- build.rs
+++ build.rs
@@ -1,15 +1,5 @@
 fn main() {
-    let arch = match std::env::var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
-        "x86_64" => "x64",
-        "x86" => "x86",
-        "arm" => "arm",
-        "aarch64" => "arm64",
-        unexpected => panic!(
-            "Unexpected `{}` architecture set by `CARGO_CFG_TARGET_ARCH`",
-            unexpected
-        ),
+    windows::build! {
+        Windows::Win32::UI::WindowsAndMessaging::MessageBoxW,
     };
-    println!("cargo:rustc-link-search=native=.windows/{}", arch);
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=.windows");
 }
