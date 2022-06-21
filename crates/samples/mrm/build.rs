7ef031103a19e08b11b362f7bf81be277e728b0b
Rafael Rivera <rafael@withinwindows.com>
2022-06-21 09:33:09 -0700

    Update xamlapp sample (#45)

--- crates/samples/mrm/build.rs
+++ crates/samples/mrm/build.rs
@@ -5,5 +5,5 @@ fn main() {
     ::windows_app::build::embed_manifest(
         r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
            <assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1" />"#,
-    )
+    );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ extern crate windows;
 pub mod core;
 
 #[allow(non_snake_case)]
+#[allow(unused_variables)]
+#[allow(non_camel_case_types)]
 #[allow(non_upper_case_globals)]
 #[allow(clippy::derivable_impls)]
 pub mod Microsoft;
