fb6e1effb407f76528bd884917046e8076d6042c
Rafael Rivera <rafael@withinwindows.com>
2022-06-21 09:12:56 -0700

    cargo fmt

--- crates/samples/xamlapp/build.rs
+++ crates/samples/xamlapp/build.rs
@@ -4,6 +4,6 @@ fn main() {
     // Temporary workaround for https://github.com/microsoft/WindowsAppSDK/issues/2634
     ::windows_app::build::embed_manifest(
         r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-           <assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1" />"#
+           <assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1" />"#,
     );
 }
